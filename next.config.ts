46bbd693d56926247185340f3be63d659fabecee
Duc Anh <anhpld@hacvntech.com>
2026-08-21 16:25:06 +0700

    cbhange

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "img.vietqr.io",
-        pathname: "/image/**",
-      },
-    ],
+
+  outputFileTracingIncludes: {
+    "*": ["./node_modules/@swc/helpers/esm/**"],
   },
 };
 
